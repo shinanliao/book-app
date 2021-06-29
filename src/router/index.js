530cf164e88c67ce98cd325a18be6b79596d2279
Shinan Liao <shinanliao1@gmail.com>
2021-06-29 14:21:35 -0700

    adds login signup and logout

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,21 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "signup",
+    name: "Signup",
+    component: Signup,
+  },
+  {
+    path: "login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/logout",
+    name: "logout",
+    component: Logout,
   },
 ];
 
