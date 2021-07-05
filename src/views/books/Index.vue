598e6cff6649107d8f7fee56a64ba42bf00c4287
Shinan Liao <shinanliao1@gmail.com>
2021-07-05 10:51:57 -0700

    fix user_books index page to filter by user_books title, comments, description, and image

--- src/views/books/Index.vue
+++ src/views/books/Index.vue
@@ -3,16 +3,13 @@
     <h3>Search for Books:</h3>
     <form v-on:submit.prevent="findBook()">
       Title:
-      <input type="text" v-model="title" />
-      <br />
-      Author:
-      <input type="text" v-model="author" />
-      <br />
-      Genre:
-      <input type="text" v-model="genre" />
-      <br />
+      <input type="text" v-model="query" />
       <button v-on:click="findBook()">Find Book</button>
       <div v-for="book in books" v-bind:key="book.id">
+        <p>{{ book.volumeInfo.title }} </p>
+        <p>{{ book.volumeInfo.authors }} </p>
+
+      </div>
     </form>
   </div>
 </template>
@@ -24,16 +21,14 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      title: "",
-      author: "",
-      genre: "",
+      query: "",
       books: [],
     };
   },
   methods: {
     findBook: function () {
-      axios.get("/books").then((response) => {
-        console.log(response.data);
+      axios.get(`/books?query=${this.query}`).then((response) => {
+        console.log("Books", response.data);
         this.books = response.data;
       });
     },
--- src/views/user_books/Index.vue
+++ src/views/user_books/Index.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="user_books">
-    <strong>My Bookshelf</strong>
+    <h2>My Bookshelf</h2>
     <div v-for="user_book in user_books" v-bind:key="user_book.id">
-      <!-- need to fix how it displays -->
-      <p>{{ user_books }}</p>
+        <span class="image">
+        <img v-bind:src="user_book.book.image" />
+      </span>
+      <br />
+      <strong>Title:</strong>
+      <p>{{ user_book.book.title }}</p>
+      <strong>Comments:</strong>
+      <p>{{ user_book.comments }}</p>
+      <strong>Description:</strong>
+      <p>{{ user_book.book.description }}</p>
     </div>
   </div>
 </template>
