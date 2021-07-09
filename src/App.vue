dd6a13cb3cb153d154e19219c8a68a6650ed5c1c
Shinan Liao <shinanliao1@gmail.com>
2021-07-09 14:36:13 -0700

    adds functional links to header

--- src/App.vue
+++ src/App.vue
@@ -23,53 +23,34 @@
           <!-- Navigation Menu-->
           <ul class="navigation-menu">
             <li>
-              <a href="index.html">Home</a>
-            </li>
-
-            <li class="has-submenu">
-              <a href="#">Features</a>
-            </li>
-
-            <li>
-              <a href="javascript:void(0);">Lifestyle</a>
+              <router-link to="/">Home</router-link>
             </li>
             <li>
-              <a href="javascript:void(0);">Travel</a>
+              <router-link to="/login" v-if="!isLoggedIn()">Login</router-link>
             </li>
             <li>
-              <a href="javascript:void(0);">Music</a>
+              <router-link to="/signup" v-if="!isLoggedIn()">Signup</router-link>
             </li>
             <li>
-              <a href="about.html">About</a>
+              <router-link :to="`/users/${userID()}`" v-if="isLoggedIn()">My Profile</router-link>
             </li>
             <li>
-              <a href="contact.html">Contact</a>
+              <router-link to="/user_books" v-if="isLoggedIn()">My Bookshelf</router-link>
             </li>
             <li>
-              <a href="javascript:void(0);" class="toggle-search" data-target="#search-wrap">
+              <router-link to="/books" v-if="isLoggedIn()">
+                Search for a Book
                 <i class="mdi mdi-magnify noti-icon"></i>
-              </a>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/logout" v-if="isLoggedIn()">Logout</router-link>
             </li>
           </ul>
           <!-- End navigation menu-->
         </div>
       </div>
     </header>
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/books" v-if="isLoggedIn()">Search for a Book</router-link>
-      |
-      <router-link to="/signup" v-if="!isLoggedIn()">Signup</router-link>
-      |
-      <router-link to="/login" v-if="!isLoggedIn()">Login</router-link>
-      |
-      <router-link :to="`/users/${userID()}`" v-if="isLoggedIn()">My Profile</router-link>
-      |
-      <router-link to="/user_books" v-if="isLoggedIn()">My Bookshelf</router-link>
-      |
-      <router-link to="/logout" v-if="isLoggedIn()">Logout</router-link>
-    </div>
     <div v-if="flashMessage">
       {{ flashMessage }}
       <button v-on:click="flashMessage = ''">Dismiss</button>
